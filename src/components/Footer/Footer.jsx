3c11cd9a995b2f2b667ae53ebd5662bcfe546325
Lukas Tucek <sp7dy@seznam.cz>
2022-03-20 11:59:32 +0100

    footer_destructuring

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './style.css';
 
 
-const Footer = (props) => {
+const Footer = ( {year, author} ) => {
     return (
         <>   
             <footer className="footer">
-			    <p className="footer__copyright">&copy; {props.year}, {props.author}</p>
+			    <p className="footer__copyright">&copy; {year}, {author}</p>
 			    <p className="footer__disclosure">Jsme fiktivní firma, která nic neprodává a v žádném případě ani za nic neručí.</p>
 		    </footer>
         </> 
